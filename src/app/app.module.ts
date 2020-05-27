6f0669fc28857d132cf516ff71157853bb4f2dc0
Bennett <bennybenester@gmail.com>
2020-05-27 17:03:20 +0300

    :sparkles: Import RouterModule and define routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
 // Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { ProductData } from './products/product-data';
+import { InMemoryWebApiModule } from "angular-in-memory-web-api";
+import { ProductData } from "./products/product-data";
 
-import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './page-not-found.component';
+import { AppComponent } from "./app.component";
+import { WelcomeComponent } from "./home/welcome.component";
+import { PageNotFoundComponent } from "./page-not-found.component";
 
 /* Feature Modules */
-import { ProductModule } from './products/product.module';
-import { UserModule } from './user/user.module';
-import { MessageModule } from './messages/message.module';
+import { ProductModule } from "./products/product.module";
+import { UserModule } from "./user/user.module";
+import { MessageModule } from "./messages/message.module";
 
 @NgModule({
   imports: [
@@ -22,13 +23,17 @@ import { MessageModule } from './messages/message.module';
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
     ProductModule,
     UserModule,
-    MessageModule
+    MessageModule,
+    RouterModule.forRoot([
+      {
+        path: "welcome",
+        component: WelcomeComponent,
+      },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", component: PageNotFoundComponent },
+    ]),
   ],
-  declarations: [
-    AppComponent,
-    WelcomeComponent,
-    PageNotFoundComponent
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
